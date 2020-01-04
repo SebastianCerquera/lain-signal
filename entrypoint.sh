32c44b9f9a938c767d9966dc608737a0f7b72cab
Sebastian Gonzalez <sebastian2zen@gmail.com>
2020-01-04 06:28:53 -0500

    renames logs directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,11 +11,11 @@ elif [ $COMMAND == "verify" ]; then
     CODE=$(cat $COMMAND_CONF | perl -ne 'print $_ if($. == 2)')
     $SIGNAL_CLI -u $SIGNAL_USERNAME verify $CODE
 elif [ $COMMAND == "receive" ]; then
-    [ -d /small/SMALL/SIGNAL ] || mkdir /small/SMALL/SIGNAL
+    [ -d /small/SMALL/SIGNAL-LOGS ] || mkdir /small/SMALL/SIGNAL-LOGS
     COUNTER=0
     while sleep 60; do
         COUNTER=$((($COUNTER + 1)))
-        $SIGNAL_CLI -u $SIGNAL_USERNAME receive > $COUNTER
+        $SIGNAL_CLI -u $SIGNAL_USERNAME receive > /small/SMALL/SIGNAL-LOGS/$COUNTER
         cat $COUNTER | perl -ne '/Expires in: \d+ seconds/ || print $_' | perl -ne '/\[\[\/small\/SMALL\/images\/\]\[\]\]/ || print $_' | perl /usr/bin/cli.pl >> /small/SMALL/SIGNAL.org
     done
 elif [ $COMMAND == "bash" ]; then
